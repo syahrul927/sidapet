aac5afddc3b6a527ccec4571ce8ece1006b3717f
srtfk <syahrulataufikaja11@gmail.com>
2024-07-24 22:00:11 +0700

    feat: add sql dummy user

---
+++ user-dummy.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+    sidapet.`User` (
+        id,
+        name,
+        email,
+        password,
+        phoneNumber,
+        createdDate,
+        updatedDate
+    )
+VALUES
+    (
+        'clyzx9rt10006y6ahkeju694x',
+        'Fauzi',
+        'fauzi@gmail.com',
+        '$2a$10$mN6JusNNwb12jx.CmTQ8IepOiIL.6yIWAIlxDE0j63fT.wBSZgf2K',
+        '08292999999',
+        '2024-07-24 14:10:44.868000000',
+        '2024-07-24 14:10:44.868000000'
+    );
